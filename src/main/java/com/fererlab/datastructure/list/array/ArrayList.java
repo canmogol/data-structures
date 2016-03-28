8c1a5c63b939eac6d9a3e81330953524505304df
Can A. MOGOL <canmogol@gmail.com>
2016-03-28 23:20:01 +0300

    ArrayList missing methods implemented, tests added

--- src/main/java/com/fererlab/datastructure/list/array/ArrayList.java
+++ src/main/java/com/fererlab/datastructure/list/array/ArrayList.java
@@ -15,6 +15,54 @@ public final class ArrayList<T> implements QArrayList<T>, CArrayList<T>, Iterabl
     private Object[] objects;
     private int size = 0;
 
+
+    /**
+     * internal iterator class
+     */
+    class InternalIterator extends Iterator<T> {
+
+        /**
+         * index will start from size
+         */
+        private int index = getSize();
+
+        @Override
+        protected int getSize() {
+            return ArrayList.this.getSize();
+        }
+
+        @Override
+        protected Maybe<T> get(int currentIndex) {
+            return ArrayList.this.get(currentIndex);
+        }
+
+        @Override
+        protected void remove(int currentIndex) {
+            ArrayList.this.remove(currentIndex);
+        }
+
+        @Override
+        public boolean hasNext() {
+            return index > 0;
+        }
+
+        /**
+         * returns the last value in the list, this prevents array copy
+         *
+         * @return value
+         */
+        @Override
+        public T next() {
+            index--;
+            return get(index).get();
+        }
+
+        @Override
+        public void remove() {
+            remove(index);
+        }
+    }
+
     public ArrayList() {
         this(10);
     }
@@ -41,7 +89,7 @@ public final class ArrayList<T> implements QArrayList<T>, CArrayList<T>, Iterabl
 
     @Override
     public void remove(int index) {
-        if (index > 0 && index < objects.length - 1) {
+        if (index >= 0 && index <= size - 1) {
             // the object to be removed
             Object removed = objects[index];
             // set null to that index, object will be GC'd
@@ -58,42 +106,69 @@ public final class ArrayList<T> implements QArrayList<T>, CArrayList<T>, Iterabl
              *
              */
             // number of elements to move
-            int numberOfElementsToMove = size - index;
+            int numberOfElementsToMove = size - index - 1;
             // this might be the last element, if so do nothing
             if (numberOfElementsToMove > 0) {
-                System.arraycopy(objects, index, objects, index - 1, numberOfElementsToMove);
+                Object[] temp = objects;
+                objects = new Object[index + numberOfElementsToMove];
+                System.arraycopy(temp, 0, objects, 0, index);
+                System.arraycopy(temp, index + 1, objects, index, numberOfElementsToMove);
             }
+            // decrease size
+            size--;
         }
     }
 
     @Override
     public void remove(T value) {
-
+        for (int i = 0; i < size; i++) {
+            if (objects[i].equals(value)) {
+                remove(i);
+                break;
+            }
+        }
     }
 
     @Override
     public void clear() {
-
+        for (int i = 0; i < size; i++) {
+            objects[i] = null;
+        }
+        objects = new Object[step];
+        size = 0;
     }
 
     @Override
     public Iterator<T> iterator() {
-        return null;
+        return new InternalIterator();
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public Maybe<T> get(int index) {
-        return null;
+        Maybe<T> maybe = Maybe.empty();
+        if (index >= 0 && index <= size - 1) {
+            T value = (T) objects[index];
+            maybe = Maybe.create(value);
+        }
+        return maybe;
     }
 
     @Override
     public boolean contains(T value) {
-        return false;
+        boolean contains = false;
+        for (Object object : objects) {
+            if (object.equals(value)) {
+                contains = true;
+                break;
+            }
+        }
+        return contains;
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return size;
     }
 
 }
--- src/test/java/com/fererlab/datastructure/ArrayListTester.java
+++ src/test/java/com/fererlab/datastructure/ArrayListTester.java
@@ -1,25 +1,28 @@
 package com.fererlab.datastructure;
 
 import com.fererlab.datastructure.iterator.Iterator;
-import com.fererlab.datastructure.list.linked.LinkedList;
+import com.fererlab.datastructure.list.array.ArrayList;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-public class LinkedListTester {
+public class ArrayListTester {
 
     // testing value
     private final String testValueOne = "Value One";
     private final String testValueTwo = "Value Two";
     private final String testValueThree = "Value Three";
+    private final String testValueFour = "Value Four";
+    private final String testValueFive = "Value Five";
+    private final String testValueSix = "Value Six";
 
     // linked list reference to test
-    private LinkedList<String> list;
+    private ArrayList<String> list;
 
     @Before
     public void prepare() {
         // create empty list
-        list = new LinkedList<>();
+        list = new ArrayList<>(3);
     }
 
     @Test
@@ -40,10 +43,6 @@ public class LinkedListTester {
         // first one should be our value
         Assert.assertEquals(list.get(0).orElseThrow(new RuntimeException("no value found at this index")), testValueOne);
 
-        // head and tail should be our value
-        Assert.assertEquals(list.getHead().getValue(), testValueOne);
-        Assert.assertEquals(list.getTail().getValue(), testValueOne);
-
         // after remove size should be 0
         list.remove(0);
         Assert.assertEquals(list.getSize(), 0);
@@ -52,17 +51,19 @@ public class LinkedListTester {
     @Test
     public void clearTest() {
 
-        // after adding size should be 1
+        // after adding size should increase by one
         list.add(testValueOne);
         Assert.assertEquals(list.getSize(), 1);
-
-        // after remove size should be 0
-        list.remove(testValueOne);
-        Assert.assertEquals(list.getSize(), 0);
-
-        // after adding size should be 1
-        list.add(testValueOne);
-        Assert.assertEquals(list.getSize(), 1);
+        list.add(testValueTwo);
+        Assert.assertEquals(list.getSize(), 2);
+        list.add(testValueThree);
+        Assert.assertEquals(list.getSize(), 3);
+        list.add(testValueFour);
+        Assert.assertEquals(list.getSize(), 4);
+        list.add(testValueFive);
+        Assert.assertEquals(list.getSize(), 5);
+        list.add(testValueSix);
+        Assert.assertEquals(list.getSize(), 6);
 
         // after clear size should be 0
         list.clear();
@@ -73,36 +74,41 @@ public class LinkedListTester {
     @Test
     public void removeOutOfIndexTest() {
 
-        // after adding size should be 1
+        // after adding size should increase by one
         list.add(testValueOne);
         Assert.assertEquals(list.getSize(), 1);
-
-        // after adding size should be 1
         list.add(testValueTwo);
         Assert.assertEquals(list.getSize(), 2);
-
-        // after adding size should be 1
         list.add(testValueThree);
         Assert.assertEquals(list.getSize(), 3);
+        list.add(testValueFour);
+        Assert.assertEquals(list.getSize(), 4);
+        list.add(testValueFive);
+        Assert.assertEquals(list.getSize(), 5);
+        list.add(testValueSix);
+        Assert.assertEquals(list.getSize(), 6);
 
         // remove index greater than zero, this should NOT raise an exception!!!
         list.remove(99);
+
     }
 
     @Test
     public void removeOneValueTest() {
 
-        // after adding size should be 1
+        // after adding size should increase by one
         list.add(testValueOne);
         Assert.assertEquals(list.getSize(), 1);
-
-        // after adding size should be 1
         list.add(testValueTwo);
         Assert.assertEquals(list.getSize(), 2);
-
-        // after adding size should be 1
         list.add(testValueThree);
         Assert.assertEquals(list.getSize(), 3);
+        list.add(testValueFour);
+        Assert.assertEquals(list.getSize(), 4);
+        list.add(testValueFive);
+        Assert.assertEquals(list.getSize(), 5);
+        list.add(testValueSix);
+        Assert.assertEquals(list.getSize(), 6);
 
         // get initial size
         int initialSize = list.getSize();
@@ -111,31 +117,33 @@ public class LinkedListTester {
         Iterator<String> iterator = list.iterator();
         while (iterator.hasNext()) {
             String value = iterator.next();
-            if (testValueTwo.equals(value)) {
+            if (testValueFour.equals(value)) {
                 iterator.remove();
-                System.out.println("removed value = " + value);
+                System.out.println("removed = " + value);
             } else {
-                System.out.println("preserved value = " + value);
+                System.out.println("preserved = " + value);
             }
         }
-        // after removing only one value, size should be 2
+        // after removing only one value, check size
         Assert.assertEquals(list.getSize(), initialSize - 1);
     }
 
     @Test
     public void removeAllValuesTest() {
 
-        // after adding size should be 1
+        // after adding size should increase by one
         list.add(testValueOne);
         Assert.assertEquals(list.getSize(), 1);
-
-        // after adding size should be 1
         list.add(testValueTwo);
         Assert.assertEquals(list.getSize(), 2);
-
-        // after adding size should be 1
         list.add(testValueThree);
         Assert.assertEquals(list.getSize(), 3);
+        list.add(testValueFour);
+        Assert.assertEquals(list.getSize(), 4);
+        list.add(testValueFive);
+        Assert.assertEquals(list.getSize(), 5);
+        list.add(testValueSix);
+        Assert.assertEquals(list.getSize(), 6);
 
         // get iterator and remove all values
         Iterator<String> iterator = list.iterator();
